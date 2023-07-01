1fdf2a4064788da42f468f12e3e1f5a080bd2681
Majaz <majazulhaque123@gmail.com>
2023-07-01 12:24:43 +0530

    Create array for each section for suffling when move

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function spin(){
-    const element = document.getElementById("container");
-    let SelectedItem = "";
+function spin() {
+    // Fetch container div using id.
+  const element = document.getElementById("container");
+  let SelectedItem = "";
+  // Array is generated using online array generater.
+  let TA = shuffle([1770, 2130, 2490]);
+  let Myntra = shuffle([1890, 2250, 2610]);
+  let Flipkart = shuffle([1850, 2210, 2570]);
+  let Samsung = shuffle([1810, 2170, 2530]);
+  let OnePlus = shuffle([1750, 2110, 2470]);
+  let Realme = shuffle([1630, 1990, 2350]);
+  let Whirlpool = shuffle([1570, 1930, 2290]);
+  let Macbook = shuffle([1530, 1890, 2250]);
+  let Speaker = shuffle([1470, 1830, 2190]);
+  let Ipad = shuffle([1410, 1770, 2130]);
 }
